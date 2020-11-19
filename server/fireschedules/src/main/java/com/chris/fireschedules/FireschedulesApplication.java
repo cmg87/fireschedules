0e97327b048ab840815f711f263f4c6e1a32850b
Chris Stump <cstump1@uncc.edu>
2020-11-19 16:20:29 -0500

    The Beginning

---
+++ server/fireschedules/src/main/java/com/chris/fireschedules/FireschedulesApplication.java
@@ -0,0 +1,13 @@
+package com.chris.fireschedules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FireschedulesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FireschedulesApplication.class, args);
+	}
+
+}
---
+++ server/fireschedules/src/test/java/com/chris/fireschedules/FireschedulesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chris.fireschedules;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FireschedulesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
